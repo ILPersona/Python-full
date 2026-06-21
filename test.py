870ae88b747000f1673626107fd8ddae934bf1f7
ILPERSONA <kettstalin@gmail.com>
2026-06-22 00:11:14 +0300

    Нечего такого

---
+++ test.py
@@ -0,0 +1,34 @@
+# me = {
+#     'name': 'IL',
+#     'age': '17',
+#     'skills': ['python', 'git']
+# }
+
+# me['city'] = 'Ser'
+
+# for key, value in me.items():
+#     print(key, '-', value)
+    
+
+# roles = ('admin', 'operator', 'resident')
+# print(roles[1])
+# print(len(roles))
+
+tasks = ['Стирка', 'Учеба', 'Тренировка']
+
+for task in tasks:
+    print(task)
+    
+for i in range(10):
+    print(i)
+    
+for i in range(1, 6):
+    print(i)
+
+for i in range(0 ,10, 2):
+    print(i)
+    
+count = 0
+while count < 3:
+    print(count)
+    count += 1
